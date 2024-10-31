e2b45445f033220465a86979ebf93e7197c103af
andrtell <116459057+andrtell@users.noreply.github.com.>
2024-10-31 18:24:24 +0100

    wip

--- ops.sh
+++ ops.sh
@@ -1,5 +1,5 @@
-export OPS_APP=creative
+export OPS_APP=elk
 export OPS_REGISTRY=registry.tell.nu
-export OPS_REGISTRY_USER=piI5LnHy3xIn6ggJfWjk
-export OPS_PODMAN_CONNECTION=tell
+export OPS_REGISTRY_USER=jvsDM63IkhzK08mwuR713STj
+export OPS_PODMAN_CONNECTION=
 export OPS_DOMAIN=elk.tell.nu
